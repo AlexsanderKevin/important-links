79e818bb017e238d4cefbedd6921d24e393503a6
kevin alexsander <kevin13348@gmail.com>
2022-02-21 07:03:45 -0300

    minor changes in the serving code

--- important-links.js
+++ important-links.js
@@ -1,9 +1,8 @@
 const express = require("express")
 const app = express()
-const porta = 3002
 
 app.use(express.static('public'))
 
-app.listen(porta, ()=>{
-    console.log(`Running at port: ${porta}`)
+app.listen(3002, ()=>{
+    console.log(`Running at port: 3002`)
 })
